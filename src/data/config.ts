fd21d2ce0b5c7dd028893c5292330cf6666bc692
Het6518 <shahhetsanjay99.9@gmail.com>
2026-09-22 22:05:58 +0530

    data has wrong mail other than that all good

---
+++ src/data/config.ts
@@ -0,0 +1,8 @@
+export const businessConfig = {
+  brandName: "Fragrances by D'Ruaa",
+  whatsappNumber: "9408104237",
+  phone: "+91 94081 04237",
+  email: "hello@fragrancesbydruaa.com",
+  instagramUrl: "https://instagram.com/",
+  whatsappLabel: "WhatsApp",
+};
