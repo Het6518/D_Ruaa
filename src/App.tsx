e8a0e1312bc28c592e78ebfcb7e0aa2fa0f31c17
Het6518 <shahhetsanjay99.9@gmail.com>
2026-09-22 21:40:59 +0530

    just setup files and proper routing setupp in app.tsx which will be rendered basically in main

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { SignatureSplash } from "./components/SignatureSplash";
+import { About } from "./pages/About";
+import { Catalogue } from "./pages/Catalogue";
+import { Contact } from "./pages/Contact";
+import { Home } from "./pages/Home";
+import { ProductDetails } from "./pages/ProductDetails";
+
+function ScrollToTop() {
+  const { pathname } = useLocation(); // use location hook to get the current pathname provided by react router dom
+  useEffect(() => {
+    window.scrollTo(0, 0); // it will scroll to the top of the page whenever the pathname changes
+  }, [pathname]);
+  return null;
+}
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-ivory text-charcoal">
+      <SignatureSplash duration={5500} />
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/fragrances" element={<Catalogue fixedCategory="fragrance" />} />
+        <Route path="/candles" element={<Catalogue fixedCategory="candle" />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
