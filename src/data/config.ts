5b5faed8d014464c2fc8d308ff845dfd8d653d1d
Het6518 <shahhetsanjay99.9@gmail.com>
2026-09-22 22:06:23 +0530

    wronf mail

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,6 +3,6 @@ export const businessConfig = {
   whatsappNumber: "9408104237",
   phone: "+91 94081 04237",
   email: "hello@fragrancesbydruaa.com",
-  instagramUrl: "https://instagram.com/",
+  instagramUrl: "https://www.instagram.com/fragrancesbyd_ruaa/",
   whatsappLabel: "WhatsApp",
 };
